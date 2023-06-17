9a7c33ee441f6f5929108fec3c5b9d2eeedb64b0
abhin_2122003 <abhinshelly@gmail.com>
2023-06-17 15:34:24 +0530

    done

--- classStaff.cpp
+++ classStaff.cpp
@@ -7,11 +7,12 @@ protected:
     int eID;
 
 public:
+    Staff() : name(""), eID(0) {}
     Staff(const string& name, int eID) : name(name), eID(eID) {}
 
     void dispInfo() {
         cout << "Employee ID : " << eID << endl
-            << "Employee Name : " << name << endl;
+             << "Employee Name : " << name << endl;
     }
 };
 
@@ -19,12 +20,13 @@ class Teacher : public Staff {
     string sub;
 
 public:
+    Teacher() : Staff(), sub("") {}
     Teacher(const string& name, int eID, const string& sub) : Staff(name, eID), sub(sub) {}
 
     void dispInfo() {
         Staff::dispInfo();
-        cout << "Employee Type : Teacher" << endl;
-        cout << "Subject : " << sub << endl << endl;
+        cout << "Employee Type : Teacher" << endl
+             << "Subject : " << sub << endl << endl;
     }
 };
 
@@ -33,12 +35,13 @@ protected:
     int typSpeed;
 
 public:
+    Typist() : Staff(), typSpeed(0) {}
     Typist(const string& name, int eID, int typSpeed) : Staff(name, eID), typSpeed(typSpeed) {}
 
     void dispInfo() {
         Staff::dispInfo();
-        cout << "Typing Speed : " << typSpeed << endl;
-        cout << "Employee Type : Typist";
+        cout << "Typing Speed : " << typSpeed << endl
+             << "Employee Type : Typist";
     }
 };
 
@@ -46,17 +49,19 @@ class Officer : public Staff {
     string pos;
 
 public:
+    Officer() : Staff(), pos("") {}
     Officer(const string& name, int eID, const string& pos) : Staff(name, eID), pos(pos) {}
 
     void dispInfo() {
         Staff::dispInfo();
-        cout << "Employee Type : Officer" << endl;
-        cout << "Position : " << pos << endl << endl;
+        cout << "Employee Type : Officer" << endl
+             << "Position : " << pos << endl << endl;
     }
 };
 
 class Regular : protected Typist {
 public:
+    Regular() : Typist() {}
     Regular(const string& name, int eID, int typSpeed) : Typist(name, eID, typSpeed) {}
 
     void dispInfo() {
@@ -69,26 +74,161 @@ class Casual : protected Typist {
     float Dwages;
 
 public:
+    Casual() : Typist(), Dwages(0.0) {}
     Casual(const string& name, int eID, int typSpeed, float Dwages)
         : Typist(name, eID, typSpeed), Dwages(Dwages) {}
 
     void dispInfo() {
         Typist::dispInfo();
-        cout << "- Casual" << endl;
-        cout << "Daily Wages : " << Dwages <<" RS/-"<< endl << endl;
+        cout << "- Casual" << endl
+             << "Daily Wages : " << Dwages << " RS/-" << endl << endl;
     }
 };
 
 int main() {
-    Teacher T1("Naruto", 1, "C++");
-    Regular R1("Saske", 2, 80);
-    Casual C1("Itachi", 3, 90, 2000);
-    Officer O1("Light", 4, "Vice Chairman");
+    int nt, np, ntr, ntc;
+    cout << "Enter the number of teachers: ";
+    cin >> nt;
+    cout << "Enter the number of officers: ";
+    cin >> np;
+    cout << "Enter the number of regular typists: ";
+    cin >> ntr;
+    cout << "Enter the number of casual typists: ";
+    cin >> ntc;
 
-    T1.dispInfo();
-    R1.dispInfo();
-    C1.dispInfo();
-    O1.dispInfo();
+    
+    Regular* R = new Regular[ntr];
+    Casual* C = new Casual[ntc];
+    Officer* O = new Officer[np];
+    Teacher* T = new Teacher[nt];
+
+    for (int i = 0; i < nt; i++) {
+        string name, subject;
+        int eID;
+        cout << "Enter Teacher " << (i + 1) << " details:" << endl;
+        cout << "Name: ";
+        cin.ignore();
+        getline(cin, name);
+        cout << "Employee ID: ";
+        cin >> eID;
+        cout << "Subject: ";
+        cin.ignore();
+        getline(cin, subject);
+        T[i] = Teacher(name, eID, subject);
+    }
+
+    for (int i = 0; i < ntr; i++) {
+        string name;
+        int eID, typSpeed;
+        cout << "Enter Regular Typist " << (i + 1) << " details:" << endl;
+        cout << "Name: ";
+        cin.ignore();
+        getline(cin, name);
+        cout << "Employee ID: ";
+        cin >> eID;
+        cout << "Typing Speed: ";
+        cin >> typSpeed;
+        R[i] = Regular(name, eID, typSpeed);
+    }
+
+    for (int i = 0; i < ntc; i++) {
+        string name;
+        int eID, typSpeed;
+        float Dwages;
+        cout << "Enter Casual Typist " << (i + 1) << " details:" << endl;
+        cout << "Name: ";
+        cin.ignore();
+        getline(cin, name);
+        cout << "Employee ID: ";
+        cin >> eID;
+        cout << "Typing Speed: ";
+        cin >> typSpeed;
+        cout << "Daily Wages: ";
+        cin >> Dwages;
+        C[i] = Casual(name, eID, typSpeed, Dwages);
+    }
+
+    for (int i = 0; i < np; i++) {
+        string name, position;
+        int eID;
+        cout << "Enter Officer " << (i + 1) << " details:" << endl;
+        cout << "Name: ";
+        cin.ignore();
+        getline(cin, name);
+        cout << "Employee ID: ";
+        cin >> eID;
+        cout << "Position: ";
+        cin.ignore();
+        getline(cin, position);
+        O[i] = Officer(name, eID, position);
+    }
+
+    int choice;
+    do {
+        cout << "View details of /-" << endl
+            << "1. Teachers" << endl
+            << "2. Officers" << endl
+            << "3. Typist" << endl
+            << "0. Exit" << endl
+            << "Enter choice: ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1: {
+            for (int i = 0; i < nt; i++) {
+                cout << "\nTeacher " << (i + 1);
+                T[i].dispInfo();
+            }
+            break;
+        }
+        case 2: {
+            for (int i = 0; i < np; i++) {
+                cout << "\nOfficer " << (i + 1);
+                O[i].dispInfo();
+            }
+            break;
+        }
+        case 3: {
+            cout << "1. Regular" << endl
+                << "2. Casual" << endl
+                << "Select: ";
+            int sel;
+            cin >> sel;
+
+            switch (sel) {
+            case 1: {
+                for (int i = 0; i < ntr; i++) {
+                    cout << "\nRegular Typist " << (i + 1);
+                    R[i].dispInfo();
+                }
+                break;
+            }
+            case 2: {
+                for (int i = 0; i < ntc; i++) {
+                    cout << "\nCasual Typist " << (i + 1);
+                    C[i].dispInfo();
+                }
+                break;
+            }
+            default: {
+                cout << "Invalid Selection!" << endl;
+            }
+            }
+            break;
+        }
+        case 0: {
+            exit(0);
+        }
+        default: {
+            cout << "Invalid Choice!" << endl;
+        }
+        }
+    } while (choice != 0);
+
+    delete[] T;
+    delete[] R;
+    delete[] C;
+    delete[] O;
 
     return 0;
 }
