8e9acbaa21a2e22951e35d8553ff94fe9a6e4b27
abhin_2122003 <abhinshelly@gmail.com>
2023-06-18 13:23:15 +0530

    done

--- classStaff.cpp
+++ classStaff.cpp
@@ -1,3 +1,9 @@
+/*30. An educational institution wishes to maintain a database of its employees. The database is
+divided into a number of classes whose hierarchical relationships are shown in the following
+figure.The figure also shows the minimum information required for each class. Specify all the classes
+and define functions to create the database and retrieve individual information as and when
+required.*/
+
 #include <iostream>
 #include <string>
 
