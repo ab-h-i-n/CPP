c49dfbc926b20829a1f596ae8fd1fa62b8caede9
abhin_2122003 <abhinshelly@gmail.com>
2023-06-18 18:50:46 +0530

    done

---
+++ digitalLibrary.cpp
@@ -0,0 +1,4 @@
+/*37. Write a program to design a class representing the information regarding digital library
+(books, tape: book & tape should be separate classes having the base class as media ). The
+class should have the functionality for adding new item, issuing, deposit etc. the program
+should use the runtime polymorphism.*/
