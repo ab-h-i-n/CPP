f9ffd73630d2a0145ea14f8ed2eac4a89a9516ef
abhin_2122003 <abhinshelly@gmail.com>
2023-06-25 21:15:34 +0530

    done

--- classMatrix.cpp
+++ classMatrix.cpp
@@ -1,4 +1,4 @@
-/*42. Write a program for developing a matrix class which can handle integer matrices of different
+/*41. Write a program for developing a matrix class which can handle integer matrices of different
 dimensions. Also overload the operator for addition, multiplication & comparison of
 matrices.*/
 
