7b7511eb84ec933c4e1a40abe88faf68d737b011
abhin_2122003 <abhinshelly@gmail.com>
2023-06-25 17:57:26 +0530

    done

--- complexaddmul.cpp
+++ complexaddmul.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 
 class Complex{
@@ -13,7 +14,12 @@ public:
     }
 
     void disp(){
-        cout<<real<<" + i"<<img;
+        cout<<real;
+        if(img>0)
+            cout<<" + i";
+        else 
+            cout<<" - i";
+        cout<<abs(img);
     }
 
     Complex operator+ (Complex c){
@@ -23,10 +29,10 @@ public:
         return (temp);
     }
 
-    Complex operator* (Complex c){
+    Complex operator* (Complex c){  /*(a +ib) * (c + id) = (ac – bd) + i(ad + bc)*/
         Complex temp;
-        temp.real = real * c.real;
-        temp.img = img * c.img;
+        temp.real = real * c.real - img * c.img;
+        temp.img = real * c.img + img * c.real;
         return(temp);
     }
 };
@@ -39,13 +45,18 @@ int main(){
     float c, d ; cin>>c>>d;
 
     Complex c1(a,b) , c2(c,d);
+
+    cout<<"\nComplex number 1 : ";
+    c1.disp();
+    cout<<"\nComplex number 2 : ";
+    c2.disp();
     
     int choice;
     Complex c3;
 
     do{
 
-        cout<<"\nOpertaions........"<<endl
+        cout<<"\n\nOpertaions........"<<endl
             <<"1.Addition"<<endl
             <<"2.Multiplication"<<endl
             <<"3.Exit"<<endl<<endl
